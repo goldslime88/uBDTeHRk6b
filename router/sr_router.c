ffee61fb2cd7b03986427a0103c444b3b313c448
mininet <lic062@eng.ucsd.edu>
2015-06-05 13:04:35 -0700

    Done!

--- router/sr_router.c
+++ router/sr_router.c
@@ -81,15 +81,17 @@ void sr_handlepacket(struct sr_instance* sr,
   /* fill in code here */
   /* define ARP or IP packet*/
   uint16_t eType = ethertype(packet);
-  /* It's ARP */
+  
   if(eType == 0x0806){
+    /* It's ARP */
     sr_handlearp(sr, packet, len, interface); 
   }
   else if (eType == 0x0800){
+    /* It's IP */
     sr_handleip(sr, packet, len, interface); 
   }
 
-  /* It's IP */
+  
 
 }/* end sr_ForwardPacket */
 
@@ -138,6 +140,7 @@ void sr_handlearp (struct sr_instance* sr,
     if(getReq != NULL){
       /* forwarding! */
       struct sr_packet* pPacket = getReq->packets;
+
       while(pPacket != NULL){
         uint8_t* outPacket = (uint8_t*) malloc(pPacket->len);
         memcpy(outPacket, pPacket->buf, pPacket->len);
@@ -167,18 +170,12 @@ void sr_handlearp (struct sr_instance* sr,
           }
           ifList = ifList->next;
         }
-
         sr_send_packet(sr, outPacket, pPacket->len, if_walker->name);
         pPacket = pPacket->next;
-
       }
-
     }
     sr_arpreq_destroy(&(sr->cache), getReq);
-    
-
     return;
-
   }
 } 
 
@@ -198,6 +195,15 @@ void sr_handleip(struct sr_instance* sr,
   /* compare */
   if (ipdrIn->ip_sum == cksum (ipdrNoSum, sizeof(sr_ip_hdr_t)-2)){
     /*the check sum is right*/
+
+    if(ipdrIn->ip_ttl == 1){
+      /* if ttl is zero */
+      sr_send_icmp3(sr, packet, len, 11, 0, interface);
+      return;
+    }
+        
+
+
     /*check if it is for me*/
     uint32_t destIp = ntohl(ipdrIn->ip_dst);
     int flagForMe = 0;
@@ -205,6 +211,7 @@ void sr_handleip(struct sr_instance* sr,
     while(tempIfList != NULL){
       if (ntohl(tempIfList->ip) == destIp){
         flagForMe = 1;
+        break;
       }
       tempIfList = tempIfList->next;
     }
@@ -216,7 +223,6 @@ void sr_handleip(struct sr_instance* sr,
       if(ipProtocol == 0x0001){
         /* it's ICMP */
         /* create packet */
-        
         uint8_t *outPacket = (uint8_t*) malloc(len);
 
         /* create out icmp not 3 header */
@@ -229,19 +235,18 @@ void sr_handleip(struct sr_instance* sr,
         memcpy(icmpNoSum+2, (uint8_t*)sendIcmp+4, len-sizeof(sr_ethernet_hdr_t)-sizeof(sr_ip_hdr_t)-4);
         sendIcmp->icmp_sum = cksum(icmpNoSum, len-sizeof(sr_ethernet_hdr_t)-sizeof(sr_ip_hdr_t)-2);
         
-
         /* create out ip header */
         sr_ip_hdr_t *sendIp = (uint8_t*) malloc(sizeof(sr_ip_hdr_t));
         memcpy(sendIp, packet+sizeof(sr_ethernet_hdr_t), sizeof(sr_ip_hdr_t));
-        uint32_t tempIp = sendIp->ip_src;
-        sendIp->ip_src = sendIp->ip_dst;
-        sendIp->ip_dst = tempIp;
+        uint32_t tempIp = sendIp->ip_dst;
+        sendIp->ip_dst = sendIp->ip_src;
+        sendIp->ip_src = tempIp;
+        
         uint8_t* ipNoSum = (uint8_t*) malloc(sizeof(sr_ip_hdr_t)-2);
         memcpy(ipNoSum, (uint8_t*)sendIp, sizeof(sr_ip_hdr_t)-10);
         memcpy(ipNoSum+sizeof(sr_ip_hdr_t)-10, (uint8_t*)sendIp+sizeof(sr_ip_hdr_t)-8, 8);
         sendIp->ip_sum = cksum (ipNoSum, sizeof(sr_ip_hdr_t)-2);
         
-
         /* fill send ether header */
         sr_ethernet_hdr_t *sendEhdr = (uint8_t *)malloc(sizeof(sr_ethernet_hdr_t));
         memcpy(sendEhdr, packet, sizeof(sr_ethernet_hdr_t));
@@ -250,20 +255,24 @@ void sr_handleip(struct sr_instance* sr,
         memcpy(sendEhdr->ether_dhost, sendEhdr->ether_shost, ETHER_ADDR_LEN);
         memcpy(sendEhdr->ether_shost, tempEthAddr, ETHER_ADDR_LEN);
         
-
         /* fill packet */
         memcpy(outPacket, sendEhdr, sizeof(sr_ethernet_hdr_t));
         memcpy(outPacket+sizeof(sr_ethernet_hdr_t), sendIp, sizeof(sr_ip_hdr_t));
         memcpy(outPacket+sizeof(sr_ethernet_hdr_t)+sizeof(sr_ip_hdr_t), 
           sendIcmp, len-sizeof(sr_ethernet_hdr_t)-sizeof(sr_ip_hdr_t));
-        /*print_hdrs(outPacket,len);*/
+        
         sr_send_packet(sr, outPacket, len, interface);
+        return;
+      }
+      else{
+        /* it may udp or tcp */
+        /* send icmp3 back */
+        sr_send_icmp3(sr, packet, len, 3, 3, interface);
         return;
       }
     }
     else{
       /* this packet is not for me */
-
       /* LPM */
       struct sr_rt* selectedSr = LPM(destIp, sr);
       if(selectedSr != NULL){
@@ -272,12 +281,11 @@ void sr_handleip(struct sr_instance* sr,
         struct sr_arpentry * findEntry = sr_arpcache_lookup(&(sr->cache), htonl(destIp));
         if(findEntry == NULL){
           /* not in cache, add arp request in queue*/
-          sr_arpcache_queuereq(&(sr->cache), htonl(destIp), packet, len, interface);
-                                       
-
+          sr_arpcache_queuereq(&(sr->cache), htonl(destIp), packet, len, interface);                                     
         }
         else{
           /* forwarding */
+
 
           /* get out-interface */
           struct sr_if* outIf = sr_get_interface(sr, selectedSr->interface);
@@ -296,7 +304,15 @@ void sr_handleip(struct sr_instance* sr,
           sr_ip_hdr_t* sendIp = (uint8_t*) malloc(sizeof(sr_ip_hdr_t));
           memcpy((uint8_t*)sendIp, outPacket+sizeof(sr_ethernet_hdr_t), 
             sizeof(sr_ip_hdr_t));
+          /* check ttl */ 
+          /*if(sendIp->ip_ttl == 1){
+            sr_send_icmp3(sr, packet, len, 11, 0, interface);
+            free(findEntry);
+            return;
+          }*/
           sendIp->ip_ttl = sendIp->ip_ttl - 1;
+
+
           sendIp->ip_sum = 0;
           sendIp->ip_sum = cksum(sendIp, sizeof(sr_ip_hdr_t));
 
@@ -306,35 +322,23 @@ void sr_handleip(struct sr_instance* sr,
           sr_send_packet(sr, outPacket, len, outIf->name);
 
           free(findEntry);
-
-
         }
-
-      /* get eth by this and ifname is selectedSr->interface*/   
-      /* struct sr_arpentry * sr_arpcache_lookup(sr->cache, htonl(destIp));*/
-
-
       }
       else{
         /* rtable ip isn't matched */
         /* send ICMP network unreachable back */
         sr_send_icmp3(sr, packet, len, 3, 0, interface);
+        
       }
-
-      
-    
-
-
-
-      
     }
-  }
+  }     
   else{
     printf("wrong checksum\n");
+    return;
   }
-
 }
 
+/* this func is for calculate LPM */
 struct sr_rt* LPM(uint32_t ip, struct sr_instance *sr){
   struct sr_rt *temproutingtable = sr->routing_table;
   int counter = 0;
@@ -379,6 +383,7 @@ struct sr_rt* LPM(uint32_t ip, struct sr_instance *sr){
   return result;
 }
 
+/* this func is for send icmp3 not icmp  */
 void sr_send_icmp3(struct sr_instance *sr, uint8_t * packet, unsigned int len, uint8_t icmp_type,
   uint8_t icmp_code, char* interface){
   int len1 = sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t) + sizeof(sr_icmp_t3_hdr_t);
@@ -394,9 +399,23 @@ void sr_send_icmp3(struct sr_instance *sr, uint8_t * packet, unsigned int len, u
   memcpy(sendIp, packet+sizeof(sr_ethernet_hdr_t), sizeof(sr_ip_hdr_t));
   sendIp->ip_len = htons(sizeof(sr_ip_hdr_t)+ sizeof(sr_icmp_t3_hdr_t));
   sendIp->ip_p = ip_protocol_icmp;
+
+  /*  loop for its if */
+  struct sr_if* ifList = sr->if_list;
+  uint32_t tempIp = 0;
+  while(ifList != NULL){
+    if(sendIp->ip_dst == ifList->ip){
+      tempIp = ifList->ip;
+      break;
+    }
+    ifList = ifList->next;
+  }
+  if(tempIp == 0){
+    struct sr_if* myIf = sr_get_interface(sr, interface);
+    tempIp = myIf->ip;
+  }
   sendIp->ip_dst = sendIp->ip_src;
-  struct sr_if* myIf = sr_get_interface(sr, interface);
-  sendIp->ip_src = myIf->ip;
+  sendIp->ip_src = tempIp;
   sendIp->ip_ttl = INIT_TTL;
   sendIp->ip_sum = 0;
   sendIp->ip_sum = cksum(sendIp, sizeof(sr_ip_hdr_t));
@@ -414,9 +433,4 @@ void sr_send_icmp3(struct sr_instance *sr, uint8_t * packet, unsigned int len, u
     sizeof(sr_icmp_t3_hdr_t));
 
   sr_send_packet(sr,outPacket,len1,interface);
-
-
-
-
-
 }
